4c8024dc12a10f432d4eea415f78b6c739d993c2
G. Nicholas D'Andrea <nick@gnidan.org>
2012-10-16 17:03:02 -0400

    readme

---
+++ README.mk
@@ -0,0 +1,12 @@
+Gematria
+========
+
+wxPython Application to calculate Gematria values. <https://en.wikipedia.org/wiki/Gematria>
+
+Run Gematria with `python app.py` or build a Mac native application with `python setup.py py2app`
+
+Features
+--------
+
+Gematria uses a sqlite database of words (words.db) and their corresponding
+Gematria values. Users can add words, delete words, search for words to get their values, or search for values to find all words with a given value.
